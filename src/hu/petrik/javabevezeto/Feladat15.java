e762f1031913ad821ee2a55e8bdde7be057a06d7
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-13 16:27:27 +0200

    Feladat 15 - Kész

--- src/hu/petrik/javabevezeto/Feladat15.java
+++ src/hu/petrik/javabevezeto/Feladat15.java
@@ -8,6 +8,8 @@ public class Feladat15 {
         for (int i = 0; i < tomb.length; i++) {
             tomb[i] = Math.random() * 200 - 100;
         }
-
+        for (int i = 1; i < tomb.length; i += 2) {
+            System.out.print((tomb[i] * tomb[i]) + " ");
+        }
     }
 }

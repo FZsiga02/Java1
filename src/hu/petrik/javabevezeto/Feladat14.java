c610c848346ca41a15af562d57b131a9d7cbd97c
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-13 15:46:10 +0200

    Feladat 14 - Menü kiíratása, menüpont bekérése

--- src/hu/petrik/javabevezeto/Feladat14.java
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -1,5 +1,6 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Feladat14 {
@@ -7,5 +8,18 @@ public class Feladat14 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Kérem adjon meg egy szöveget: ");
         String szoveg = sc.nextLine();
+        String menuPont = "";
+        while (!menuPont.equals("f")) {
+            System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel: ");
+            System.out.println("\t");
+            System.out.println("\ta - Nagybetűssé alakítani");
+            System.out.println("\tb - Kisbetűssé alakítani");
+            System.out.println("\tc - Lekérdezni a hosszát");
+            System.out.println("\td - Összehasonlítani egy másik szöveggel");
+            System.out.println("\te - A szöveg egy részét kiíratni");
+            System.out.println("\tf - Kilépni");
+            System.out.print("Adja meg a menüpontot: ");
+            menuPont = sc.nextLine().toLowerCase();
+        }
     }
 }

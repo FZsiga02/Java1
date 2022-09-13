a35e637eada862b3b288ca5df3d0de9b8cd48e7b
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-13 16:17:26 +0200

    Feladat 15 - 25 elemű valós értékeket tartalmazó tömb

---
+++ src/hu/petrik/javabevezeto/Feladat15.java
@@ -0,0 +1,9 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Scanner;
+
+public class Feladat15 {
+    public static void main(String[] args) {
+        double[] tomb = new double[25];
+    }
+}
